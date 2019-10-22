7d6b692c92cf739891b31035fb028263d51c87cd
MRudi95 <marvin.rudolph@htwg-konstanz.de>
2019-10-22 18:21:58 +0200

    added more routes and commands; changed index to about

--- app/controllers/SlayController.scala
+++ app/controllers/SlayController.scala
@@ -14,7 +14,7 @@ class SlayController @Inject()(cc: ControllerComponents) extends AbstractControl
   }
 
   def about = Action {
-    Ok(views.html.index())
+    Ok(views.html.about())
   }
 
   def slay = Action {
@@ -32,4 +32,36 @@ class SlayController @Inject()(cc: ControllerComponents) extends AbstractControl
     Slay.tui.processInput("mov " + coord1 + " " + coord2)
     Ok(slayAsText)
   }
+
+  def cmb(coord1: String, coord2: String) = Action{
+    Slay.tui.processInput("cmb " + coord1 + " " + coord2)
+    Ok(slayAsText)
+  }
+
+  def plc(coord: String) = Action {
+    Slay.tui.processInput("plc " + coord)
+    Ok(slayAsText)
+  }
+
+  def undo() = Action {
+    Slay.tui.processInput("undo")
+    Ok(slayAsText)
+  }
+
+  def redo() = Action {
+    Slay.tui.processInput("redo")
+    Ok(slayAsText)
+  }
+
+  def end() = Action {
+    Slay.tui.processInput("end")
+    Ok(slayAsText)
+  }
+
+  def surrender() = Action {
+    Slay.tui.processInput("ff20")
+    Ok(slayAsText)
+  }
 }
+
+
