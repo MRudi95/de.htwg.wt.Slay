65344c881f75a52584a47252b4a0157814fe4489
MRudi95 <marvin.rudolph@htwg-konstanz.de>
2021-01-16 19:44:31 +0100

    einiges unnützes wegrationalisiert

--- app/controllers/SlayController.scala
+++ app/controllers/SlayController.scala
@@ -2,54 +2,36 @@ package controllers
 
 import javax.inject._
 import play.api.mvc._
-import de.htwg.se.slay.{Slay, SlayModule}
+import de.htwg.se.slay.SlayModule
 import de.htwg.se.slay.controller.controllerComponent._
 import de.htwg.se.slay.model.fileIOComponent.fileIoJSONimpl.FileIO
 import de.htwg.se.slay.util.Observer
-import play.api.libs.json.{JsString, JsValue, Json}
+import play.api.libs.json.Json
 import play.api.libs.streams.ActorFlow
 import akka.actor._
 import akka.stream.Materializer
 import com.google.inject.{Guice, Injector}
-import de.htwg.se.slay.aview.TextUI
 
 @Singleton
 class SlayController @Inject()(cc: ControllerComponents) (implicit system: ActorSystem, mat: Materializer) extends AbstractController(cc){
-  val injector: Injector = Guice.createInjector(new SlayModule)
-  val (gameController, tui) = newGameInstance()
-
-  var message :String = _
-  var updateEvent: Event = _
   val jsonIO = new FileIO
+  val injector: Injector = Guice.createInjector(new SlayModule)
 
-  def newGameInstance():(ControllerInterface, TextUI) = {
+  val gameController = newGameInstance() //wird spaeter liste mit allen laufenden spielen
+
+  def newGameInstance():ControllerInterface = {
     val controller: ControllerInterface = injector.getInstance(classOf[ControllerInterface])
-    val tui = new TextUI(controller)
 
     controller.createGrid("Map1")
     controller.nextturn()
 
-    (controller, tui)
+    controller
   }
 
+
+  //wird noch wegrationalisiert
   def slayAsText = {
-    views.html.slay(this, message)
-  }
-
-  def checksuccess: Boolean = {
-    updateEvent match {
-      case _: SuccessEvent => true
-      case _ => false
-    }
-  }
-
-  def jsonUpdate = {
-    if (checksuccess)
-      Ok(jsonIO.gridToJson(gameController.grid, gameController.players))
-    else
-      Ok(Json.obj(
-        "message" -> message,
-      ))
+    views.html.slay(this, "")
   }
 
   def about = Action {
@@ -59,62 +41,54 @@ class SlayController @Inject()(cc: ControllerComponents) (implicit system: Actor
   def slay = Action {
     Ok(slayAsText)
   }
+  // /wird noch wegrationalisiert
 
   //commands
   def buy(coord: String) = Action {
-    tui.processInput("buy " + coord)
-    jsonUpdate
+    processInput("buy " + coord, gameController)
+    Ok("")
   }
 
   def mov(coord1: String, coord2: String) = Action{
-    tui.processInput("mov " + coord1 + " " + coord2)
-    jsonUpdate
+    processInput("mov " + coord1 + " " + coord2, gameController)
+    Ok("")
   }
 
   def cmb(coord1: String, coord2: String) = Action{
-    tui.processInput("cmb " + coord1 + " " + coord2)
-    jsonUpdate
+    processInput("cmb " + coord1 + " " + coord2, gameController)
+    Ok("")
   }
 
   def plc(coord: String) = Action {
-    tui.processInput("plc " + coord)
-    jsonUpdate
+    processInput("plc " + coord, gameController)
+    Ok("")
   }
 
   def bal(coord: String) = Action {
-    tui.processInput("bal " + coord)
-    jsonUpdate
+    processInput("bal " + coord, gameController)
+    Ok("")
   }
 
   def undo() = Action {
-    tui.processInput("undo")
-    Ok(slayAsText)
+    processInput("undo", gameController)
+    Ok("")
     //brauch noch javascript wie die anderen
   }
 
   def redo() = Action {
-    tui.processInput("redo")
-    Ok(slayAsText)
+    processInput("redo", gameController)
+    Ok("")
     //brauch noch javascript wie die anderen
   }
 
   def end() = Action {
-    tui.processInput("end")
-    Ok(Json.obj("player" -> Json.obj(
-        "playername" -> getPlayerturn(),
-        "playercolor" -> getPlayercolor(),
-      )
-    ))
+    processInput("end", gameController)
+    Ok("")
   }
 
   def surrender() = Action {
-    tui.processInput("ff20")
-    Ok(slayAsText)
-  }
-
-  def command(command: String) = Action{
-    tui.processInput(command)
-    Ok(slayAsText)
+    processInput("ff20", gameController)
+    Ok("")
   }
 
   def getJson() = Action{
@@ -161,7 +135,6 @@ class SlayController @Inject()(cc: ControllerComponents) (implicit system: Actor
 
     val jsonIO = new FileIO
     override def update(e: Event): Boolean = {
-      updateEvent = e
       e match{
         case _: SuccessEvent => out ! jsonIO.gridToJson(gameController.grid, gameController.players).toString(); true
         case _: MoneyErrorEvent => out ! Json.obj( "message" -> "Not enough Money!").toString(); true
@@ -185,6 +158,49 @@ class SlayController @Inject()(cc: ControllerComponents) (implicit system: Actor
           )).toString(); true
         case _ => false
       }
+    }
+  }
+
+  //taken from TextUI
+  def processInput(input: String, controller: ControllerInterface): Unit = {
+    val coord = "[A-Z]\\d+".r
+    val bal = s"bal ($coord)".r
+    val buy = s"buy ($coord)".r
+    val plc = s"plc ($coord)".r
+    val mov = s"mov ($coord) ($coord)".r
+    val cmb = s"cmb ($coord) ($coord)".r
+
+    input match {
+      case "q" =>
+      case "quit" =>
+      case "undo" => controller.undo()
+      case "redo" => controller.redo()
+      case "end" => controller.nextturn()
+      case "ff20" => controller.surrender()
+      case bal(c) if checkIndex(c) =>
+        controller.seeBalance(convertIndex(c))
+      case buy(c) if checkIndex(c) =>
+        controller.buyPeasant(convertIndex(c))
+      case plc(c) if checkIndex(c) =>
+        controller.placeCastle(convertIndex(c))
+      case mov(c1, c2) if checkIndex(c1) && checkIndex(c2) =>
+        controller.moveUnit(convertIndex(c1), convertIndex(c2))
+      case cmb(c1, c2) if checkIndex(c1) && checkIndex(c2) =>
+        controller.combineUnit(convertIndex(c1), convertIndex(c2))
+      case _ => println("Wrong Input!")
+    }
+
+    //helper functions
+    def checkIndex(idx: String): Boolean ={
+      val coord = "([A-Z])(\\d+)".r
+      val coord(cols, rows) = idx
+      val col = cols.charAt(0).toInt - 65
+      if(col > controller.grid.colIdx || rows.toInt - 1 > controller.grid.rowIdx) false else true
+    }
+    def convertIndex(idx: String): Int ={
+      val cols = idx.charAt(0).toInt - 65
+      val rows = idx.charAt(1).asDigit - 1
+      rows * (controller.grid.colIdx+1) + cols
     }
   }
 }
